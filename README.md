ddae41ba8cf25e137dd62154731990b1874cd9cf
Carter Peene <carter.peene@alleninstitute.org>
2024-08-01 17:54:38 +0000

    edit readme, remove prints

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# aind-capsule-template
+# NWB-Packaging-CCF
 
-Basic structure of a capsule to be customized as needed.
+This Capsule is intended to take an existing NWB file and CSVs containing probe CCF information, and modify the NWB *electrodes* table to include this CCF information. In addition to requiring the NWB to exist in the data directory, it requires a **CCF* asset containing a .CSV file for each probe (usually 6). 
+
+Each CSV is expected to contain the probe name within its filename, of the form `probeX` and to have the following columns in this order:
+**channel_id, structure_acronym, structure_id, x_coord, y_coord, z_coord, horizontal_position, vertical_position, is_valid, cortical_depth**.
+
+Importantly, all probes whose that were used to package the inputted NWB file and produce the electrodes table, must have .CSVs present in the input! Otherwise, updating the electrodes table will fail because some electrodes rows will not have coordinate information to add.
+
+The NWB File may be in Zarr or HDMF format.
--- code/run_capsule.py
+++ code/run_capsule.py
@@ -12,30 +12,37 @@ data_folder = Path("../data/")
 scratch_folder = Path("../scratch/")
 results_folder = Path("../results/")
 
-# converts channel name of the form 'LFP1' to idx of the form 0
+# converts channel name of the form 'LFP1' or 'AP1' to idx of the form 0
 def channel_name_to_idx(channel_name):
-    return int(channel_name[3:])-1
+    stripped_name = channel_name.replace('LFP','').replace('AP','')
+    try:
+        return int(stripped_name)-1
+    except:
+        raise Exception('Unexpected channel name format')
 
 
-def format_probe_name(probe_name):
+def probe_name_from_file_name(csv_path):
+    probe_name = None
+    for string_segment in csv_path.stem.split('_'):
+        if string_segment.lower().startswith('probe'):
+            probe_name = string_segment
+            break
+    if not probe_name:
+        raise Exception('This file name does not appear to contain a probe name')
     return probe_name[0].upper() + probe_name[1:-1] + probe_name[-1].upper()
-
-
-def get_channel_idxs(electrodes, probe_names,):
-    channel_idxs = {}
-    for row in enumerate(electrodes):
-        print(row['id'])
 
 
 def build_ccf_map(probe_csvs):
     ccf_map = {}
-    for probe_csv_name in probe_csvs:
-        probe_name = format_probe_name(probe_csv_name.stem.split('_')[2])
-        probe_info = list(csv.reader(open(probe_csv_name)))
+    for probe_csv_path in probe_csvs:
+        print('Reading',probe_csv_path)
+        probe_name = probe_name_from_file_name(probe_csv_path)
+        probe_info = list(csv.reader(open(probe_csv_path)))
         for channel_id, structure, structure_id, x, y, z, horz, vert, valid, cort_depth in probe_info[1:]:
             ccf_map[probe_name, int(channel_id)] = [structure, x,y,z]
 
     return ccf_map
+
 
 def get_new_electrode_colums(nwb, ccf_map):
     locs, xs, ys, zs = [], [], [], []
@@ -51,6 +58,7 @@ def get_new_electrode_colums(nwb, ccf_map):
         ys.append(y)
         zs.append(z)
     return locs, xs, ys, zs
+
 
 def run():
     probe_csvs = [p for p in (data_folder/'ccf').iterdir() if p.name.endswith('.csv')]
@@ -77,28 +85,26 @@ def run():
         io_class = NWBHDF5IO
     print(f"NWB backend: {NWB_BACKEND}")
 
-    read_io = io_class(str(nwbfile_input_path), mode='a')
-    nwb = read_io.read()
-
-
-    print(len(nwb.electrodes))
-    print(nwb.electrodes[0])
-    print(nwb.electrodes[1])
-    print(nwb.electrodes[3])
-
+    print('Building CCF Map from .CSVs')
     ccf_map = build_ccf_map(probe_csvs)
-    print(ccf_map.keys())
-    locs, xs, ys, zs = get_new_electrode_colums(nwb, ccf_map)
 
-    nwb.electrodes.location.data[:] = np.array(locs)
-    nwb.electrodes.add_column('x', 'ccf x coordinate', data=xs)
-    nwb.electrodes.add_column('y', 'ccf y coordinate', data=ys)
-    nwb.electrodes.add_column('z', 'ccf z coordinate', data=zs)
+    print('Reading NWB in append mode:', nwbfile_input_path)
+    with io_class(str(nwbfile_input_path), mode='a') as read_io:
+        nwb = read_io.read()
 
-    nwbfile_output_path = results_folder / f"{nwbfile_input_path.stem}.nwb"
-    with io_class(str(nwbfile_output_path), "w") as export_io:
-        export_io.export(src_io=read_io, nwbfile=nwb, write_args={'link_data': False})
-    print(f"Done writing {nwbfile_output_path}")
+        print('Getting new electrode columns')
+        locs, xs, ys, zs = get_new_electrode_colums(nwb, ccf_map)
+
+        nwb.electrodes.location.data[:] = np.array(locs)
+        nwb.electrodes.add_column('x', 'ccf x coordinate', data=xs)
+        nwb.electrodes.add_column('y', 'ccf y coordinate', data=ys)
+        nwb.electrodes.add_column('z', 'ccf z coordinate', data=zs)
+
+        nwbfile_output_path = results_folder / f"{nwbfile_input_path.stem}.nwb"
+        print('Exporting to NWB:',nwbfile_output_path)
+        with io_class(str(nwbfile_output_path), "w") as export_io:
+            export_io.export(src_io=read_io, nwbfile=nwb, write_args={'link_data': False})
+        print(f"Done writing {nwbfile_output_path}")
 
 
 if __name__ == "__main__": run()
